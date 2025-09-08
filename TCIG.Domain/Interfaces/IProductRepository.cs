c6bd4b9057845954f112d91ad8ec679112ef86c0
7aib <zohaibrana03@gmail.com>
2025-09-08 19:16:35 +0500

    :

---
+++ TCIG.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCIG.Domain.Entities;
+
+namespace TCIG.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductModel>> GetAllProductsAsync();
+        Task<ProductModel?> GetProductsbyIdAsync(int id);
+        Task<IEnumerable<ProductModel>> AddProductsAsync(ProductModel product);
+        Task<ProductModel> UpdateProductsAsync(ProductModel product);
+        Task<bool> DeleteProductsAsync(int id);
+
+    }
+}
---
+++ TCIG.Infrastructure/Repositories/ProductRepoitory.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TCIG.Domain.Entities;
+using TCIG.Domain.enums;
+using TCIG.Domain.Interfaces;
+using TCIG.Infrastructure.Data;
+
+namespace TCIG.Infrastructure.Repositories
+{
+    public class ProductRepoitory(AppDbContext dbContext) : IProductRepository
+    {
+        public async Task<IEnumerable<ProductModel>> GetAllProductsAsync()
+        {
+            return await Task.FromResult(dbContext.Products.ToList());
+        }
+
+        public async Task<ProductModel?> GetProductsbyIdAsync(int id)
+        {
+            return await dbContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<IEnumerable<ProductModel>> AddProductsAsync(ProductModel product)
+        {
+            product.CreatedDate = DateTime.UtcNow;
+            product.Status = Enums.ProductStatus.Active;
+
+            dbContext.Products.Add(product);
+
+            await dbContext.SaveChangesAsync();
+
+            return await Task.FromResult(dbContext.Products.ToList());
+        }
+
+        public async Task<ProductModel> UpdateProductsAsync(ProductModel product)
+        {
+            var existingProduct = await dbContext.Products.FirstOrDefaultAsync(x => x.Id == product.Id);
+            if (existingProduct != null)
+            {
+                existingProduct.ProductName = product.ProductName;
+                existingProduct.Price = product.Price;
+                existingProduct.Stock = product.Stock;
+                existingProduct.Status = product.Status;
+                existingProduct.UpdatedDate = DateTime.UtcNow;
+                await dbContext.SaveChangesAsync();
+                return existingProduct;
+            }
+            throw new InvalidOperationException($"Product with Id {product.Id} not found.");
+        }
+
+        public async Task<bool> DeleteProductsAsync(int id)
+        {
+            var existingProduct = await dbContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingProduct != null)
+            {
+                dbContext.Products.Remove(existingProduct);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        } 
+
+
+
+    }
+}
